deb6604e7845c58be703dcabc98b1fa78a174174
AlexSikand <alex@sikand.org>
2019-10-09 02:32:22 -0400

    Initial Commit

---
+++ BUBusTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BUBusTracker
+//
+//  Created by Alex Sikand on 10/9/19.
+//  Copyright © 2019 AlexSikand. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
